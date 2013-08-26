d5ac52d5c1f3418f7cd4fc83915b8ead9a1cf97f
theteedog <theteedog@users.noreply.github.com>
2013-08-26 12:30:53 +1000

    Update public-bootstrap.sh

--- public-bootstrap.sh
+++ public-bootstrap.sh
@@ -14,7 +14,7 @@ python setup.py install
 
 #Download Private Bootstrap package.
 curl -L https://raw.github.com/theteedog/chef-bootstrap-linux/master/boto-s3-download.py -o /tmp/boto-s3-download.py
-python /tmp/boto-s3-download.py -b '$1' -k '$2'
+python /tmp/boto-s3-download.py -b "$1" -k "$2"
 unzip /tmp/private-bootstrap.zip -d /tmp/private-bootstrap/
 
 #Execute the private Linux Bootstrap
