9b30c91b581b149be31d43291803a418cb7bc9f5
theteedog <theteedog@users.noreply.github.com>
2013-08-26 10:42:57 +1000

    Update boto-s3-download.py

--- boto-s3-download.py
+++ boto-s3-download.py
@@ -5,7 +5,7 @@ def main(argv):
    bucketName = ''
    objectKey = ''
    try:
-      opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+      opts, args = getopt.getopt(argv,"hb:k:",["bucketName=","objectKey:="])
    except getopt.GetoptError:
       print 'boto-s3-download.py -b <bucketName> -k <ObjectKey>'
       sys.exit(2)
@@ -17,19 +17,17 @@ def main(argv):
          bucketName = arg
       elif opt in ("-k", "--ObjectKey"):
          objectKey = arg
-   print 'S3 Bucket Name is "', inputfile
-   print 'S3 Object Key  is "', outputfile
-   download(
+   print 'S3 Bucket Name is "', bucketName
+   print 'S3 Object Key  is "',objectKey
+   download(bucketName,objectKey)
 
-def download(bucketName,s3ObjectKey)   
+def download(bucketName,s3ObjectKey):
   c = boto.connect_s3()
   b = c.get_bucket(bucketName) # substitute your bucket name here
   from boto.s3.key import Key
-  
   k = Key(b)
   k.key = s3ObjectKey
-  k.get_contents_to_filename('/tmp/')
+  k.get_contents_to_filename('/tmp/private-bootstrap.zip')
 
 if __name__ == "__main__":
    main(sys.argv[1:])
-   
